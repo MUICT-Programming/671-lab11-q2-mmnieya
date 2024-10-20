c0f1ccc6f51e75f643c647242e20f8c6573a2b95
Jeeratchaya Phuiuta <jeeratchaya.phu@student.mahidol.edu>
2024-10-20 21:46:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# YOUR CODE HERE
+n = int(input())
+def summation(a,b,c) :
+    for i in range(n) :
+        sum = a[i] + b[i]
+        c.append(sum)
+    return sum
+def find_min_max(a) :
+    a.sort()
+    print(f”({a[0]}, {a[n-1]})”)
+    return a
+lst1 = []
+lst2 = []
+update = []
+for i in range(2*n) :
+    num = int(input())
+    if i <n :
+        lst1.append(num)
+    else :
+        lst2.append(num)
+
+summation(lst1,lst2,update)
+print(update)
+find_min_max(update)
